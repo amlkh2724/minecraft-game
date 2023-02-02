a6e7d615fc2b7c5e7fe82d9b12d42938107979e7
amal khanjer <amlkh2724@gmail.com>
2023-02-02 23:01:23 +0200

    build the render board

---
+++ js/board.js
@@ -0,0 +1,56 @@
+
+const buildBoard = document.querySelector("#game-board");
+const matrixSize = 21;
+
+export function createBoard() {
+    for (let i = 0; i < matrixSize; i++) {
+        for (let j = 0; j < matrixSize; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("sky");
+            getDirt(cell, i);
+            getGrass(cell, i);
+            buildTree(cell, i, j)
+            renderStone(cell,i,j)
+            buildBoard.appendChild(cell);
+        }
+    }
+}
+function renderStone(cell,i,j){
+    if (i < 15 && i > 11 && j > 6 && j < 15){
+        cell.classList.remove("sky")
+        cell.classList.add("rockk")
+    }
+
+}
+
+function buildTree(cell, i, j) {
+    if (i < 15 && i > 10 && j > 16 && j < 18) {
+        cell.classList.remove("sky")
+        cell.classList.add("trunkk")
+    }
+    if (
+        (i < 11 && i > 7 && j > 15 && j < 19) ||
+        (i < 15 && i > 11 && j > 2 && j < 6)
+
+
+    ) {
+        cell.classList.remove("sky")
+        cell.classList.add("leaves")
+
+
+    }
+}
+
+
+function getGrass(cell, i) {
+    if (i === 15) {
+        cell.classList.remove("sky")
+        cell.classList.add("grasss")
+    }
+}
+function getDirt(cell, i) {
+    if (i > 15) {
+        cell.classList.remove("sky");
+        cell.classList.add("dirtt");
+    }
+}
---
+++ js/game.js
@@ -0,0 +1,14 @@
+import { createBoard as createBoardGame } from "./board.js";
+
+function createBoard() {
+    createBoardGame();
+  }
+  createBoard()
+  
+//   function gameLoop() {
+//     startGame();
+//     requestAnimationFrame(gameLoop);
+//   }
+  
+//   requestAnimationFrame(gameLoop);
+  
