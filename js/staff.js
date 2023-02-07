46809088788f654f9268ea2635619db66243981b
amal khanjer <amlkh2724@gmail.com>
2023-02-07 21:43:06 +0200

    fixed

--- js/staff.js
+++ js/staff.js
@@ -1,5 +1,3 @@
-
-
 const buildBoard = document.getElementById("game-board");
 const pickaxeClick = document.querySelector("#pickaxe");
 const axeClick = document.querySelector("#axe");
@@ -23,7 +21,6 @@ const swordaaa = document.querySelector(".containerSword")
 const sword1 = document.querySelector(".sword")
 
 
-
 function addedClass() {
     counterDirt.classList.add("red")
     counterGrass.classList.add("red")
@@ -33,8 +30,6 @@ function addedClass() {
     counterLeaves.classList.add("red")
 }
 addedClass()
-
-
 
 function restartColors() {
     counterDirt.classList.remove("green");
@@ -47,117 +42,102 @@ function restartColors() {
 
 }
 
-
-function restartBlurColor() {
-    pickaxe.classList.remove("blue");
-    shovel.classList.remove("blue");
-    axe.classList.remove("blue");
-}
-function addedClass() {
-    counterDirt.classList.add("red")
-    counterGrass.classList.add("red")
-    counterRock.classList.add("red")
-    counterSnow.classList.add("red")
-    counterTrunk.classList.add("red")
-    counterLeaves.classList.add("red")
-}
-addedClass()
-
-
-
-function restartColors() {
-    counterDirt.classList.remove("green");
-    counterGrass.classList.remove("green");
-    counterRock.classList.remove("green");
-    counterSnow.classList.remove("green");
-    counterTrunk.classList.remove("green");
-    counterLeaves.classList.remove("green");
-    swordaaa.classList.remove("blue")
-
-}
-
-
 function restartBlurColor() {
     pickaxe.classList.remove("blue");
     shovel.classList.remove("blue");
     axe.classList.remove("blue");
 }
 
-let counter = {
-    dirt: 0,
-    rock: 0,
-    grass: 0,
-    leaves: 0,
-    trunk: 0,
-    snow: 0
-};
+const blocks = {
 
-const updateCounter = (counterName, value) => {
-    counter[counterName] += value;
-};
-const removeMaterial = (target, material) => {
-    target.classList.remove(material);
-    updateCounter(material, 1);
-};
+    rock: {
+        className: "rock",
+        counter: 0,
+        counterElement: counterRock
+    },
+    dirt: {
+        className: "dirt",
+        counter: 0,
+        counterElement: counterDirt
+    },
+    grass: {
+        className: "grass",
+        counter: 0,
+        counterElement: counterGrass
+    },
+    leaves: {
+        className: "leaves",
+        counter: 0,
+        counterElement: counterLeaves
+    },
+    trunk: {
+        className: "trunk",
+        counter: 0,
+        counterElement: counterTrunk
+    },
+    snow: {
+        className: "snow",
+        counter: 0,
+        counterElement: counterSnow
+    },
 
-const addMaterial = (target, element) => {
 
-    if (counter[element] > 0) {
-        counter[element]--;
-        if (target.classList.contains("sky") || !target.classList.length) {
-            target.classList.add(element);
-        }
-    }
+};
+const tools = {
+    pickaxe: ["rock", "snow"],
+    axe: ["trunk", "leaves"],
+    shovel: ["dirt", "grass"],
 };
 
 buildBoard.addEventListener("click", (e) => {
     const target = e.target;
+
     if (swordaaa.classList.contains("blue")) {
         target.classList = "";
     }
-    if (pickaxeClick.classList.contains("blue")) {
-        if (target.classList.contains("rock")) {
-            removeMaterial(target, "rock");
-        }
-        if (target.classList.contains("snow")) {
-            removeMaterial(target, "snow");
-        }
-    }
-    if (shovelClick.classList.contains("blue")) {
-        if (target.classList.contains("dirt")) {
-            removeMaterial(target, "dirt");
-        }
-        if (target.classList.contains("grass")) {
-            removeMaterial(target, "grass");
-        }
-    }
-    if (axeClick.classList.contains("blue")) {
-        if (target.classList.contains("leaves")) {
-            removeMaterial(target, "leaves");
-        }
-        if (target.classList.contains("trunk")) {
-            removeMaterial(target, "trunk");
-        }
-    }
 
-    if (getDirt.classList.contains("green")) {
+    for (const blockName in blocks) {
+        const block = blocks[blockName];
+        if (pickaxe.classList.contains("blue")) {
+            tools.pickaxe.forEach(blockName => {
+                if (target.classList.contains(blockName)) {
+                    const block = blocks[blockName];
+                    block.counter++;
+                    block.counterElement.innerHTML = block.counter;
+                    target.classList.remove(block.className);
+                }
+            });
+        }
 
-        addMaterial(target, "dirt");
-    }
-    if (getTrunk.classList.contains("green")) {
-        addMaterial(target, "trunk");
-    }
-    if (getRock.classList.contains("green")) {
-        addMaterial(target, "rock");
-    }
-    if (getLeaves.classList.contains("green")) {
-        addMaterial(target, "leaves");
-    }
-    if (getGrass.classList.contains("green")) {
-        addMaterial(target, "grass");
-    }
-    if (getSnow.classList.contains("green")) {
-        addMaterial(target, "snow");
+        if (shovel.classList.contains("blue")) {
+            tools.shovel.forEach(blockName => {
+                if (target.classList.contains(blockName)) {
+                    const block = blocks[blockName];
+                    block.counter++;
+                    block.counterElement.innerHTML = block.counter;
+                    target.classList.remove(block.className);
+                }
+            });
+        }
+
+        if (axe.classList.contains("blue")) {
+            tools.axe.forEach(blockName => {
+                if (target.classList.contains(blockName)) {
+                    const block = blocks[blockName];
+                    block.counter++;
+                    block.counterElement.innerHTML = block.counter;
+                    target.classList.remove(block.className);
+                }
+            });
+        }
+
+        if (block.counterElement.classList.contains("green") && block.counter > 0) {
+            block.counter--;
+            block.counterElement.innerHTML = block.counter;
+            if (target.classList.contains("sky") || !target.classList.length) {
+                target.classList.add(block.className);
+            }
+        }
     }
 });
 
@@ -178,46 +158,48 @@ axeClick.addEventListener("click", (e) => {
     restartColors()
 
 });
+
 shovelClick.addEventListener("click", (e) => {
     restartBlurColor()
     shovel.classList.add("blue");
     restartColors()
 
 });
+
 getDirt.addEventListener("click", () => {
-    restartColors();
-    counterDirt.classList.add("green");
-    restartBlurColor();
+    restartColors()
+    counterDirt.classList.add("green")
+    restartBlurColor()
 
-});
 
+})
 getTrunk.addEventListener("click", () => {
-    restartColors();
-    counterTrunk.classList.add("green");
-    restartBlurColor();
-});
+    restartColors()
+    counterTrunk.classList.add("green")
+    restartBlurColor()
+})
 
 getRock.addEventListener("click", () => {
-    restartColors();
-    counterRock.classList.add("green");
-    restartBlurColor();
-});
+    restartColors()
+    counterRock.classList.add("green")
+    restartBlurColor()
+})
 
 getLeaves.addEventListener("click", () => {
-    restartColors();
-    counterLeaves.classList.add("green");
-    restartBlurColor();
-});
+    restartColors()
+    counterLeaves.classList.add("green")
+    restartBlurColor()
+})
 
 getGrass.addEventListener("click", () => {
-    restartColors();
-    counterGrass.classList.add("green");
-    restartBlurColor();
-});
+    restartColors()
+    counterGrass.classList.add("green")
+    restartBlurColor()
+
+})
 
 getSnow.addEventListener("click", () => {
-    restartColors();
-    counterSnow.classList.add("green");
-    restartBlurColor();
-});
-
+    restartColors()
+    counterSnow.classList.add("green")
+    restartBlurColor()
+})
